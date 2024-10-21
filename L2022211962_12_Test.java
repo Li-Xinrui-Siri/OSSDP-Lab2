ae0a2d3caa6bbc6cfe99b7df14903298775836a9
Li-Xinrui-Siri <2825382552@qq.com>
2024-10-21 18:59:20 +0800

    Add files via upload
    L2022211962_12_Test

--- L2022211962_12_Test.java
+++ L2022211962_12_Test.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class L2022211962_12_Test {
+    /**
+     * 测试最一般的情况
+     */
     @Test
     public void testMultiply_ValidInputs() {
         Solution solution = new Solution();
@@ -11,6 +14,9 @@ public class L2022211962_12_Test {
         assertEquals("56088", solution.multiply("123", "456"));
     }
 
+    /**
+     * 测试特殊情况：含0式
+     */
     @Test
     public void testMultiply_ZeroInput() {
         Solution solution = new Solution();
@@ -19,6 +25,9 @@ public class L2022211962_12_Test {
         assertEquals("0", solution.multiply("0", "0"));
     }
 
+    /**
+     * 测试特殊情况：不超过数据类型上限前提下的较大值
+     */
     @Test
     public void testMultiply_LargeNumbers() {
         Solution solution = new Solution();
